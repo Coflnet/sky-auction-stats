8e035463376d2bd4835866efbeed62b3199949c8
Florian Mühlhans <muehlhans.florian@web.de>
2022-06-19 19:20:40 +0200

    changed kafka message consuming

--- internal/kafka/auction.go
+++ internal/kafka/auction.go
@@ -6,51 +6,74 @@ import (
 	"github.com/Coflnet/auction-stats/internal/redis"
 	"github.com/rs/zerolog/log"
 	"github.com/segmentio/kafka-go"
+	"sync"
 	"time"
 )
 
 func ReadAuctions() error {
-	m, err := auctionReader.FetchMessage(context.Background())
-	if err != nil {
-		return err
+
+	messages := make([]kafka.Message, 0)
+	for i := 0; i < 100; i++ {
+		m, err := auctionReader.FetchMessage(context.Background())
+		if err != nil {
+			return err
+		}
+		messages = append(messages, m)
 	}
 
-	err = processMessage(&m)
-	if err != nil {
-		log.Error().Err(err).Msgf("error processing message")
-		return err
-	}
+	processedMessages := processMessages(messages)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
 	defer cancel()
-	err = auctionReader.CommitMessages(ctx, m)
+	for processedMessage := range processedMessages {
+		err := auctionReader.CommitMessages(ctx, processedMessage)
+		if err != nil {
+			log.Error().Err(err).Msgf("error committing message")
+			return err
+		}
+	}
 
 	return nil
 }
 
 // processMessage takes a new-auction message from kafka and processes everything that has to be done
 // example key of a message: 7807253170724460696506/09/2022 12:23:12
-func processMessage(msg *kafka.Message) error {
+func processMessages(messages []kafka.Message) <-chan kafka.Message {
 
-	uuid := string(msg.Key)[:20]
-	t := string(msg.Key)[len(string(msg.Key))-len("06/14/2022 04:55:56"):]
+	ch := make(chan kafka.Message)
+	wg := sync.WaitGroup{}
+	for _, m := range messages {
+		wg.Add(1)
 
-	timestamp, err := time.Parse("01/02/2006 15:04:05", t)
-	if err != nil {
-		log.Error().Err(err).Msgf("can not convert timestamp: %s", t)
-		return err
+		go func(msg kafka.Message) {
+			defer wg.Done()
+
+			uuid := string(msg.Key)[:20]
+			t := string(msg.Key)[len(string(msg.Key))-len("06/14/2022 04:55:56"):]
+
+			timestamp, err := time.Parse("01/02/2006 15:04:05", t)
+			if err != nil {
+				log.Error().Err(err).Msgf("can not convert timestamp: %s", t)
+				return
+			}
+
+			auction := model.Auction{
+				Start: timestamp,
+				UUID:  uuid,
+			}
+
+			err = redis.CountAuction(&auction)
+			if err != nil {
+				log.Error().Err(err).Msgf("error counting auction")
+
+				return
+			}
+
+			ch <- msg
+		}(m)
 	}
 
-	auction := model.Auction{
-		Start: timestamp,
-		UUID:  uuid,
-	}
-
-	err = redis.CountAuction(&auction)
-	if err != nil {
-		log.Error().Err(err).Msgf("error counting auction")
-		return err
-	}
-
-	return nil
+	wg.Wait()
+	close(ch)
+	return ch
 }
